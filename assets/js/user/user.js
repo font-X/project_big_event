380bb2be6acdb042caafe877172a6e65d8ff402f
be_reborn-zhang <1759891981@qq.com>
2020-10-15 18:50:53 +0800

    全部完成

---
+++ assets/js/user/user.js
@@ -0,0 +1,50 @@
+$(function () {
+    var form = layui.form
+    var layer = layui.layer;
+    form.verify({
+        nickname: function (value) {
+            if (value.length > 6) {
+                return "昵称长度必须在1 ~ 6 个字符之间！"
+            }
+        }
+    })
+    initUserInfo()
+
+    // 初始化用户的基本信息
+    function initUserInfo() {
+        $.ajax({
+            method: 'GET',
+            url: '/my/userinfo',
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg("获取用户信息失败！")
+                }
+                // console.log(res)
+                // 调用form.val()快速为表单赋值
+                form.val('formUserInfo', res.data)
+            }
+        })
+
+    }
+
+    $("#btnReset").on("click", function (e) {
+        e.preventDefault()
+        initUserInfo()
+        // 发起ajax请求
+        $.ajax({
+            method: 'POST',
+            url: '/my/userinfo',
+            data: $('.layui-form').serialize(),
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('更新用户信息失败！')
+                }
+                layer.msg('更新用户信息成功！')
+                console.log(window.parent);
+
+                // 调用父页面的方法，重新渲染用户的头像和用户的自信息
+                window.parent.getUserInfo()
+            }
+        })
+    })
+})
